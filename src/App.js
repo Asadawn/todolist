bf5eabb8999e265dc1fdfba92abe0d7d96e6e3c9
Asad Awan <asadawn3@gmail.com>
2023-09-14 00:45:11 +0500

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Header  from './components/Header';
+import Form from './components/Form';
+import Todolist from './components/Todolist';
+const App=() =>{ 
+  const [input,setInput]= useState('');
+  const [todos,setTodos]= useState([]);
+  const [editTodo,setEditTodo]= useState(null);
+  return(
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div >
+          <Header/>
+          </div>
+          <div>
+          <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          />
+          </div>
+          <div>
+            <Todolist todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
+          </div>
+        </div>
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
